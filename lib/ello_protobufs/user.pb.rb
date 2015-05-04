963e278480874e250a803ced6ef51133b57b968c
Bob Bonifield <bobbonifield@gmail.com>
2015-05-04 11:36:53 -0600

    user->name is optional

--- lib/ello_protobufs/user.pb.rb
+++ lib/ello_protobufs/user.pb.rb
@@ -20,7 +20,7 @@ module ElloProtobufs
     required :uint32, :id, 1
     required :string, :href, 2
     required :string, :username, 3
-    required :string, :name, 4
+    optional :string, :name, 4
     required :uint64, :created_at, 5
     required :uint64, :updated_at, 6
   end
