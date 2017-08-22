340f6f0f336a67e7f068214998e5220bacd67368
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-08-22 13:27:06 -0600

    Update href for AI submission

--- spec/factories.rb
+++ spec/factories.rb
@@ -77,7 +77,7 @@ FactoryGirl.define do
     skip_create
     id { generate(:protobuf_id) }
     title { 'Artist Invite Title' }
-    href { "http://ello.co/artist-invites/my-slug" }
+    href { "notifications/artist-invites/my-slug" }
     created_at { Time.now.to_i }
     updated_at { Time.now.to_i }
   end
